43118ba042cbdda968094591992bdc7033f383b8
Toasted-3 <haydenprewett@outlook.com>
2026-01-15 20:55:14 +0000

    added password list UI element

--- PasswordManager/Main.Designer.cs
+++ PasswordManager/Main.Designer.cs
@@ -31,45 +31,55 @@
             AddPassword = new Button();
             ChangePassword = new Button();
             RemovePassword = new Button();
+            PasswordList = new ListBox();
             SuspendLayout();
             // 
             // AddPassword
             // 
-            AddPassword.Location = new Point(25, 15);
+            AddPassword.Location = new Point(25, 12);
             AddPassword.Name = "AddPassword";
-            AddPassword.Size = new Size(249, 36);
+            AddPassword.Size = new Size(236, 36);
             AddPassword.TabIndex = 0;
             AddPassword.Text = "Add a password";
             AddPassword.UseVisualStyleBackColor = true;
             // 
             // ChangePassword
             // 
-            ChangePassword.Location = new Point(280, 15);
+            ChangePassword.Location = new Point(277, 12);
             ChangePassword.Name = "ChangePassword";
-            ChangePassword.Size = new Size(249, 36);
+            ChangePassword.Size = new Size(236, 36);
             ChangePassword.TabIndex = 1;
             ChangePassword.Text = "Change a password";
             ChangePassword.UseVisualStyleBackColor = true;
             // 
             // RemovePassword
             // 
-            RemovePassword.Location = new Point(535, 15);
+            RemovePassword.Location = new Point(535, 12);
             RemovePassword.Name = "RemovePassword";
-            RemovePassword.Size = new Size(249, 36);
+            RemovePassword.Size = new Size(236, 36);
             RemovePassword.TabIndex = 2;
             RemovePassword.Text = "Remove a password";
             RemovePassword.UseVisualStyleBackColor = true;
             // 
-            // Form1
+            // PasswordList
+            // 
+            PasswordList.FormattingEnabled = true;
+            PasswordList.Location = new Point(25, 86);
+            PasswordList.Name = "PasswordList";
+            PasswordList.Size = new Size(746, 344);
+            PasswordList.TabIndex = 3;
+            // 
+            // Main
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(PasswordList);
             Controls.Add(RemovePassword);
             Controls.Add(ChangePassword);
             Controls.Add(AddPassword);
-            Name = "Form1";
-            Text = "Form1";
+            Name = "Main";
+            Text = "Password Manager";
             ResumeLayout(false);
         }
 
@@ -78,5 +88,6 @@
         private Button AddPassword;
         private Button ChangePassword;
         private Button RemovePassword;
+        private ListBox PasswordList;
     }
 }
